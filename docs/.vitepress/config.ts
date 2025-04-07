ec157465b3cbc97fae8e453e602aa839f27bb0d0
YunYouJun <me@yunyoujun.cn>
2025-04-07 15:44:09 +0800

    chore: init basic structure

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,89 @@
+import type { DefaultTheme } from 'vitepress'
+import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
+import { defineConfig } from 'vitepress'
+import { groupIconMdPlugin } from 'vitepress-plugin-group-icons'
+import { version } from '../../package.json'
+import vite from './vite.config'
+
+const GUIDES: DefaultTheme.NavItemWithLink[] = [
+  { text: 'Getting Started', link: '/guide/' },
+  { text: 'Installation & Usage', link: '/guide/install' },
+]
+
+const VERSIONS: (DefaultTheme.NavItemWithLink | DefaultTheme.NavItemChildren)[] = [
+  { text: `v${version} (current)`, link: '/' },
+  { text: `Release Notes`, link: 'https://github.com/YunYouJun/pkg-placeholder/releases' },
+]
+
+export default defineConfig({
+  title: 'pkg-placeholder',
+  description: '_description_',
+  markdown: {
+    theme: {
+      light: 'vitesse-light',
+      dark: 'vitesse-dark',
+    },
+    codeTransformers: [
+      transformerTwoslash(),
+    ],
+    languages: ['js', 'jsx', 'ts', 'tsx'],
+    config: (md) => {
+      md.use(groupIconMdPlugin)
+    },
+  },
+  cleanUrls: true,
+  vite,
+  themeConfig: {
+    // logo: '/logo.svg',
+    nav: [
+      {
+        text: 'Guide',
+        items: [
+          {
+            items: GUIDES,
+          },
+        ],
+      },
+      {
+        text: `v${version}`,
+        items: VERSIONS,
+      },
+    ],
+    sidebar: {
+      '/': [
+        {
+          text: 'Guide',
+          items: GUIDES,
+        },
+      ],
+    },
+    editLink: {
+      pattern: 'https://github.com/YunYouJun/pkg-placeholder/edit/main/docs/:path',
+      text: 'Suggest changes to this page',
+    },
+    search: {
+      provider: 'local',
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/YunYouJun/pkg-placeholder' },
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2025-PRESENT YunYouJun.',
+    },
+  },
+
+  head: [
+    // ['meta', { name: 'theme-color', content: '#ffffff' }],
+    // ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }],
+    ['meta', { name: 'author', content: 'YunYouJun' }],
+    // ['meta', { property: 'og:title', content: '' }],
+    // ['meta', { property: 'og:image', content: '' }],
+    // ['meta', { property: 'og:description', content: '_description_' }],
+    // ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    // ['meta', { name: 'twitter:image', content: '' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, viewport-fit=cover' }],
+  ],
+})
